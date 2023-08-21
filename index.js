db9bc40a0ce12784ec126e9aa43d4ad4f0be7de8
Omotayo <kehindeomotayo11@gmail.com>
2023-08-21 14:43:42 +0100

    Done

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+  const changeMeElement = document.getElementById("text");
+  changeMeElement.textContent = "This is really cool!";
+});
